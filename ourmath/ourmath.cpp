66fb0b2738c13c620172f961b401df4003fa3c8d
fiufnvanfiufn <vanyachich@outlook.com>
2025-02-13 12:46:46 +0300

    add new lib

---
+++ ourmath/ourmath.cpp
@@ -0,0 +1,7 @@
+#include "ourmath.hpp"
+
+namespace ourmath {
+    int RemainderOfDevision(int devisible, int devider) {
+        return devisible % devider;
+    }
+} // namespace ourmath
---
+++ ourmath/ourmath.hpp
@@ -0,0 +1,8 @@
+#ifndef OURMATH_HPP
+#define OURMATH_HPP
+
+namespace ourmath {
+    int RemainderOfDevision(int devisible, int devider);
+}  // namespace ourmath
+
+#endif //OURMATH_HPP
