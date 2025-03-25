b47066dc846d6000326b9bd8136b356c203f10ca
fiufnvanfiufn <vanyachich@outlook.com>
2025-03-25 21:28:10 +0300

    add build

---
+++ build/road/CMakeFiles/road.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/road.dir/road.cpp.o"
+  "CMakeFiles/road.dir/road.cpp.o.d"
+  "libroad.a"
+  "libroad.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/road.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
